eaa1ddd547376795795f49d81fed6cf4fdb4f5b3
Sameer Ali <somethingwhichisnottaken@gmail.com>
2018-03-08 13:01:01 +0000

    Added more providers

--- pynoti/providers/iCloud.py
+++ pynoti/providers/iCloud.py
@@ -1,9 +1,9 @@
 from ..app.SMTPEmail import SMTPEmail
 
-class Hotmail(SMTPEmail):
+class iCloud(SMTPEmail):
 
     def __init__(self, user, password):
         self.user = user
         self.password = password
-        self.host = 'smtp.live.com'
+        self.host = 'smtp.mail.me.com'
         self.port = 587
